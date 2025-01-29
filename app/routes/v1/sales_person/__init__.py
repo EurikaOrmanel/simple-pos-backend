5f922e2aa68445aad190e29331bdc38cec0421eb
EurikaOrmanel <wworgba@gmail.com>
2025-01-29 23:28:27 +0000

    produts router

--- app/routes/v1/sales_person/__init__.py
+++ app/routes/v1/sales_person/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .order import order_router
 from .customer import customer_router
 from .auth import sales_person_auth_router
+from .product import product_router
 
 
 sales_person_router = APIRouter(prefix="/sales_persons")
@@ -12,3 +13,6 @@ sales_person_router.include_router(sales_person_auth_router, prefix="/auth")
 sales_person_router.include_router(customer_router)
 
 sales_person_router.include_router(order_router)
+
+
+sales_person_router.include_router(product_router)
