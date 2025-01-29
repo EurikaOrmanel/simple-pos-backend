ed524edee5f395e2e4b94dadf235660fd78aa95b
EurikaOrmanel <wworgba@gmail.com>
2025-01-29 23:31:09 +0000

    sales person products preview

--- app/dependencies/controllers/sales_person/product.py
+++ app/dependencies/controllers/sales_person/product.py
@@ -2,10 +2,11 @@ from app.controllers.sales_person.product import SalesPersonProductController
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 from fastapi import Depends
+from ...db.db_session_dep import DBSessionDep
 
 
 def get_sales_person_product_controller(
-    db_session: AsyncSession,
+    db_session: DBSessionDep,
 ) -> SalesPersonProductController:
     return SalesPersonProductController(db_session)
 
